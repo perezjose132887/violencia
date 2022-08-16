9b22cdb5f2a919de9a07b7f4e118db3207f7d265
perezjose132887 <perez.jose.132887@gmail.com>
2022-08-16 10:43:53 -0400

    Validamos el email y password en el login

--- app/src/main/java/com/example/violenciacontralamujer/MainActivity.java
+++ app/src/main/java/com/example/violenciacontralamujer/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.violenciacontralamujer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     Button register,login;
+    EditText etEmail,etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +33,45 @@ public class MainActivity extends AppCompatActivity {
 
         register=(Button) findViewById(R.id.btnRegister);
         login=(Button) findViewById(R.id.btnLogin);
+        etEmail=(EditText) findViewById(R.id.etEmail);
+        etPassword=(EditText) findViewById(R.id.etPassword);
 
 
+        //validamos el email y password en el login()
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this,Contactos.class);
-                startActivity(intent);
+                StringRequest stringRequest=new StringRequest(Request.Method.POST, "http://192.168.1.100/violencia/verificarLogin.php", new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(!response.isEmpty()){
+                            Intent intent=new Intent(getApplicationContext(),Contactos.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this,"Usuario o contraseña incorrecta",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                }){
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> parametros=new HashMap<String,String>();
+                        parametros.put("email",etEmail.getText().toString());
+                        parametros.put("password",etPassword.getText().toString());
+                        return parametros;
+                    }
+                };
+                RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+                requestQueue.add(stringRequest);
             }
         });
+
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
 
 }
